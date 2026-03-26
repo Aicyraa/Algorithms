9f188e8c8dce5731515e0f1841a4fae2f9b1477a
Jee <jeecoding09@gmail.com>
2026-03-26 16:15:01 +0800

    feat: added sorting and removing dupplicates method for class "Tree"

--- binary-search/code.test.js
+++ binary-search/code.test.js
@@ -1,9 +1,17 @@
 const Tree = require('./tree.js')
 
 describe('Sorting method test', () => {
-   const newTree = new Tree()
 
    it('Array with 2 elements', () => {
-      expect((newTree.root = [4, 3])).toEqual([3, 4])
+      const tree = new Tree()
+      tree.root = [4, 3]
+      expect((tree.root)).toEqual([3, 4])
    })
+
+   it('Array with 4 elements', () => {
+      const tree = new Tree()
+      tree.root = [4, 3, 1, 2]
+      expect((tree.root)).toEqual([1, 2, 3, 4])
+   })
+
 })
--- binary-search/tree.js
+++ binary-search/tree.js
@@ -1,43 +1,42 @@
 class Tree {
    #root
 
-   #sort(arr) {
+   sort(arr) {
       if (arr.length === 1) {
          return arr
       }
 
       const mid = Math.floor(arr.length / 2)
-      const left = this.#sort(arr.slice(0, mid))
-      const right = this.#sort(arr.slice(mid))
+      const left = this.sort(arr.slice(0, mid))
+      const right = this.sort(arr.slice(mid))
 
       let l = 0,
-         r = 0,
-         result = []
+          r = 0,
+          result = []
 
       while (l < left.length && r < right.length) {
-         if (left[l] >= right[r]) {
-            result.pop(arr[l++])
+         if (left[l] <= right[r]) {
+            result.push(left[l++])
          } else {
-            result.pop(arr[r++])
+            result.push(right[r++])
          }
       }
-
-      console.log(left, right)
-      console.log(result)
-      return [result, ...left.slice(l), ...right.slice(r)]
-   }
-
-   #removeDups(arr) {
-      return arr
+      
+      return [...result, ...left.slice(l), ...right.slice(r)]
+      
    }
 
    #buildTree(arr) {
-      return arr
+      /*
+         get the middle
+         
+      
+      */
    }
 
    set root(arr) {
-      const sorted = this.#removeDups(this.#sort(arr))
-      this.#root = this.#buildTree(sorted)
+      const sorted = new Set(this.sort(arr))
+      this.#root = this.#buildTree([...sorted])
    }
 
    get root() {
