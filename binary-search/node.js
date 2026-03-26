cab6de7478dc03e09ff60b5d46a2111fea410dc0
Jee <jeecoding09@gmail.com>
2026-03-26 14:15:57 +0800

    Initial Files

---
+++ binary-search/node.js
@@ -0,0 +1,17 @@
+class Node {
+   #value
+   #left
+   #right
+
+   set data(value) {
+      this.data = value
+   }
+
+   set left(value) {
+      this.left = value
+   }
+
+   set right(value) {
+      this.right = value
+   }
+}
