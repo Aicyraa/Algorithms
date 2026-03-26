62c7eb5ebb83c5fdc485de63e3b40965741218c1
Jee <jeecoding09@gmail.com>
2026-03-26 17:39:31 +0800

    feat: a function that print the tree in a beautiful way :)

--- binary-search/tree.js
+++ binary-search/tree.js
@@ -1,14 +1,16 @@
-class Tree {
-   #root
+const Node = require('./node')
 
-   sort(arr) {
+class Tree {
+   #root = null
+
+   #sort(arr) {
       if (arr.length === 1) {
          return arr
       }
 
       const mid = Math.floor(arr.length / 2)
-      const left = this.sort(arr.slice(0, mid))
-      const right = this.sort(arr.slice(mid))
+      const left = this.#sort(arr.slice(0, mid))
+      const right = this.#sort(arr.slice(mid))
 
       let l = 0,
           r = 0,
@@ -27,16 +29,23 @@ class Tree {
    }
 
    #buildTree(arr) {
-      /*
-         get the middle
-         
-      
-      */
+
+      if (arr.length === 0 ) {
+         return null
+      }
+
+      const mid = Math.floor(arr.length / 2)
+      const root = new Node(arr[mid])
+      root.left = this.#buildTree(arr.slice(0, mid))
+      root.right = this.#buildTree(arr.slice(mid + 1))
+
+      return root
    }
 
    set root(arr) {
-      const sorted = new Set(this.sort(arr))
-      this.#root = this.#buildTree([...sorted])
+      const sorted = new Set(this.#sort(arr))
+      const root = this.#buildTree([...sorted])
+      this.#root = root
    }
 
    get root() {
