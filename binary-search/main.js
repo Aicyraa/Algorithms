2aa3fa04b9d651724f66b807420729f8bd5ffe21
Jee <jeecoding09@gmail.com>
2026-03-26 17:41:16 +0800

    feat: A method that build the BST model from a sorted array

--- binary-search/main.js
+++ binary-search/main.js
@@ -1,5 +1,15 @@
-import Tree from './tree.js'
+const Tree = require('./tree.js')
 
-const arr = [2, 5]
+function prettyPrint(node, prefix = '', isLeft = true) {
+   if (node === null || node === undefined) {
+     return;
+   }
+ 
+   prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+   console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+   prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+ }
+
 const BST = new Tree()
-BST.root = arr
+BST.root = [3, 2, 1, 6, 5, 4, 10, 9, 8, 7]
+prettyPrint(BST.root)
